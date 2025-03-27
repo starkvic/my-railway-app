10ec20b8cbe7edb8436894e41a0df215b581e055
starkvic <starkvic@github.com>
2025-03-27 19:46:52 +0300

    Miss input Miss input

--- public/hippo-sim.js
+++ public/hippo-sim.js
@@ -1,95 +1,176 @@
-let hippos = [];
-let predators = [];
-let food;
-let w = 600;
-let h = 600;
-let numHippos = 10;
-let numPredators = 5;
-let lambda0 = 0.9;
-let c = 0.02;
-let t = 0;
-let lambda;
+let cols, rows;
+let grid = [];
+let openSet = [];
+let closedSet = [];
+let start, end;
+let w = 30;
+let path = [];
+let hippo;
+let hippoImg;
+let moveSpeed = 10;
+let moveCounter = 0;
+let pathCompleted = false;
+
+function preload() {
+  hippoImg = loadImage('assets/hippo.png');
+}
 
 function setup() {
-  let canvas = createCanvas(w, h);
-  canvas.parent("sketch-holder");
-  lambda = lambda0;
+  createCanvas(600, 600);
+  cols = floor(width / w);
+  rows = floor(height / w);
 
-  for (let i = 0; i < numHippos; i++) {
-    hippos.push({
-      pos: createVector(random(width), random(height)),
-      fitness: Infinity
-    });
+  for (let i = 0; i < cols; i++) {
+    grid[i] = [];
+    for (let j = 0; j < rows; j++) {
+      grid[i][j] = new Cell(i, j);
+    }
   }
 
-  for (let i = 0; i < numPredators; i++) {
-    predators.push(createVector(random(width), random(height)));
-  }
+  start = grid[0][0];
+  end = grid[cols - 1][rows - 1];
+  start.wall = false;
+  end.wall = false;
 
-  food = createVector(random(width), random(height));
+  openSet.push(start);
+
+  hippo = {
+    idx: 0,
+    moving: false,
+    x: start.i * w,
+    y: start.j * w,
+    bobbing: 0
+  };
 }
 
 function draw() {
-  background(30, 144, 255, 100);
-  fill(0, 255, 0);
-  ellipse(food.x, food.y, 20, 20);
+  background(220);
 
-  for (let p of predators) {
-    noStroke();
-    fill(255, 0, 0, 50);
-    ellipse(p.x, p.y, 80 + sin(frameCount * 0.1) * 10);
-    fill(255, 0, 0);
-    stroke(0);
-    strokeWeight(1);
-    ellipse(p.x, p.y, 30 + sin(frameCount * 0.1) * 5);
-  }
+  if (openSet.length > 0 && !hippo.moving) {
+    let lowest = 0;
+    for (let i = 0; i < openSet.length; i++) {
+      if (openSet[i].hippoCost < openSet[lowest].hippoCost) {
+        lowest = i;
+      }
+    }
 
-  for (let h of hippos) {
-    let r = random();
+    let current = openSet[lowest];
 
-    if (r < lambda) {
-      let bestPos = getBestHippo();
-      let alpha = random(0.1, 0.5);
-      let beta = random(0.1, 0.5);
-      let awayFromPred = predatorAvoidance(h.pos);
-      h.pos.add(p5.Vector.mult(p5.Vector.sub(bestPos, h.pos), alpha));
-      h.pos.add(p5.Vector.mult(awayFromPred, beta));
+    if (current === end) {
+      hippo.moving = true;
     } else {
-      let gamma = random(0.1, 0.3);
-      h.pos.add(p5.Vector.mult(p5.Vector.sub(food, h.pos), gamma));
+      removeFromArray(openSet, current);
+      closedSet.push(current);
+
+      let neighbors = current.getNeighbors();
+      for (let neighbor of neighbors) {
+        if (!closedSet.includes(neighbor) && !neighbor.wall) {
+          let tempHippoCost = current.hippoCost + neighbor.cost;
+
+          if (!openSet.includes(neighbor) || tempHippoCost < neighbor.hippoCost) {
+            neighbor.hippoCost = tempHippoCost;
+            neighbor.previous = current;
+
+            if (!openSet.includes(neighbor)) {
+              openSet.push(neighbor);
+            }
+          }
+        }
+      }
     }
 
-    h.pos.x = constrain(h.pos.x, 0, width);
-    h.pos.y = constrain(h.pos.y, 0, height);
-
-    h.fitness = dist(h.pos.x, h.pos.y, food.x, food.y);
-    fill(r < lambda ? 100 : 0);
-    ellipse(h.pos.x, h.pos.y, 15, 15);
+    path = [];
+    let temp = current;
+    path.push(temp);
+    while (temp.previous) {
+      path.push(temp.previous);
+      temp = temp.previous;
+    }
   }
 
-  lambda = lambda0 * exp(-c * t);
-  t++;
+  drawGrid();
+  drawHippo();
 }
 
-function getBestHippo() {
-  let best = hippos[0];
-  for (let h of hippos) {
-    if (h.fitness < best.fitness) {
-      best = h;
+function drawGrid() {
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j].show();
     }
   }
-  return best.pos.copy();
+  for (let c of closedSet) {
+    c.highlight(color(255, 100, 100, 180));
+  }
+  for (let o of openSet) {
+    o.highlight(color(100, 255, 100, 180));
+  }
+  for (let p of path) {
+    p.highlight(color(0, 0, 255, 100));
+  }
 }
 
-function predatorAvoidance(pos) {
-  let avoidance = createVector(0, 0);
-  for (let p of predators) {
-    let d = dist(pos.x, pos.y, p.x, p.y);
-    if (d < 100) {
-      let away = p5.Vector.sub(pos, p);
-      away.normalize();
-      avoidance.add(away);
+function drawHippo() {
+  if (hippo.moving && !pathCompleted) {
+    if (moveCounter % moveSpeed === 0 && hippo.idx < path.length) {
+      let p = path[path.length - 1 - hippo.idx];
+      hippo.x = p.i * w + w / 2;
+      hippo.y = p.j * w + w / 2;
+      hippo.idx++;
+      if (hippo.idx >= path.length) {
+        pathCompleted = true;
+      }
     }
+    moveCounter++;
   }
-  return avoidance;
+
+  if (hippo.moving || pathCompleted) {
+    hippo.bobbing = sin(frameCount * 0.1) * 1.5;
+    noStroke();
+    fill(0, 0, 0, 50);
+    ellipse(hippo.x, hippo.y + 5, w * 0.7, w * 0.3);
+    imageMode(CENTER);
+    image(hippoImg, hippo.x, hippo.y + hippo.bobbing, w * 1.1, w * 1.1);
+  }
+}
+
+function removeFromArray(arr, elt) {
+  let idx = arr.indexOf(elt);
+  if (idx > -1) {
+    arr.splice(idx, 1);
+  }
+}
+
+class Cell {
+  constructor(i, j) {
+    this.i = i;
+    this.j = j;
+    this.wall = random(1) < 0.15;
+    this.cost = this.wall ? 50 : random(1, 10);
+    this.hippoCost = Infinity;
+    this.previous = undefined;
+  }
+
+  getNeighbors() {
+    let neighbors = [];
+    if (this.i < cols - 1) neighbors.push(grid[this.i + 1][this.j]);
+    if (this.i > 0) neighbors.push(grid[this.i - 1][this.j]);
+    if (this.j < rows - 1) neighbors.push(grid[this.i][this.j + 1]);
+    if (this.j > 0) neighbors.push(grid[this.i][this.j - 1]);
+    return neighbors;
+  }
+
+  show() {
+    noStroke();
+    if (this.wall) {
+      fill(139, 69, 19);
+    } else {
+      fill(0, 100, 255, map(this.cost, 1, 10, 50, 200));
+    }
+    rect(this.i * w, this.j * w, w - 1, w - 1);
+  }
+
+  highlight(col) {
+    fill(col);
+    rect(this.i * w, this.j * w, w - 1, w - 1);
+  }
 }
