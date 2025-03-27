f935215de28f6ca36b31b4690f24237b0cafb17f
starkvic <starkvic@github.com>
2025-03-27 19:20:34 +0300

    New change with styling

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve static files (CSS, images, JS)
 app.use(express.static(path.join(__dirname, "public")));
 
+// Routes
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views/index.html")));
 app.get("/about", (req, res) => res.sendFile(path.join(__dirname, "views/about.html")));
 app.get("/simulation", (req, res) => res.sendFile(path.join(__dirname, "views/simulation.html")));
 
-app.listen(PORT, () => console.log(`App running on port ${PORT}`));
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
